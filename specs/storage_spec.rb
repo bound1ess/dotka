cf22ee66a035a4c2190812cdd8cb2d90115fc110
Ilya <bound1ess.dev@gmail.com>
2014-12-10 19:43:08 +0600

    Implement DotkaM::Storage and specs.

---
+++ specs/storage_spec.rb
@@ -0,0 +1,24 @@
+require_relative "../src/dotka/storage"
+
+RSpec.describe DotkaM::Storage do
+
+	before :all do
+		@storage = DotkaM::Storage.new
+	end
+
+	it "returns the entire file contents" do
+		items = @storage.get "items"
+		expect(items).to be_truthy
+		expect(items).to be_an(Array)
+	end
+
+	it "returns a single row" do
+		item = @storage.get "items", {"id" => 1}
+		expect(item).to eq({
+			"id" => "1",
+			"name" => "blink",
+			"localized_name" => "Blink Dagger"
+		})	
+	end
+	
+end
---
+++ src/dotka/storage.rb
@@ -0,0 +1,21 @@
+require "json"
+
+module DotkaM
+	class Storage
+		def get file, conditions = {}
+			rows = JSON.parse File.read("#{Dir.pwd}/data/#{file}.json")
+			if not conditions.empty?
+				rows.select! { |row|
+					selected = true
+					conditions.each { |key, value|
+						if not (row.has_key? key) or not (row[key] == value.to_s)
+							selected = false
+						end
+					}
+					selected
+				}	
+			end
+			rows.length == 1 ? rows.first : rows
+		end
+	end
+end
