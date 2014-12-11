6cff2959e5ce9df49cbde41f46b62dc1b73e4ce5
Ilya <bound1ess.dev@gmail.com>
2014-12-11 13:53:10 +0600

    Complete Match.

--- specs/match_spec.rb
+++ specs/match_spec.rb
@@ -4,12 +4,28 @@ RSpec.describe DotkaM::Match do
 
 	before :all do
 		@match = DotkaM::Match.new({
-			# to be finished		
+			"match_id" => "123456789",
+			"players" => [
+				{"account_id" => "1"}
+			],		
+			"radiant_win" => false,
+			"duration" => "3600",
+			"start_time" => "123456789",
+			"lobby_type" => "0",
+			"game_mode" => "1",
+			"cluster" => "111"
 		})	
 	end
 	
 	it "provides data getters" do
-		# to be finished
+		expect(@match.id).to eq(123456789)
+		expect(@match.players.first.account_id).to eq(1)
+		expect(@match.winner).to eq("dire")
+		expect(@match.duration).to eq(3600)
+		expect(@match.start_time).to eq(123456789)
+		expect(@match.lobby.localized_name).to eq("Public matchmaking")
+		expect(@match.game_mode.localized_name).to eq("All Pick")
+		expect(@match.region.localized_name).to eq("US West")	
 	end
 	
 end
--- src/dotka/match.rb
+++ src/dotka/match.rb
@@ -1,7 +1,39 @@
 require_relative "raw"
+require_relative "player"
+require_relative "lobby"
+require_relative "game_mode"
+require_relative "region"
+require_relative "storage"
 
 module DotkaM
 	class Match < Raw
-		# to be finished
+		def id
+			@raw["match_id"].to_i
+		end
+		def players
+			players = Array.new
+			@raw["players"].each { |player|
+				players.push Player.new(player)
+			}
+			players
+		end
+		def winner
+			(!! @raw["radiant_win"]) ? "radiant" : "dire"
+		end
+		def duration
+			@raw["duration"].to_i
+		end
+		def start_time
+			@raw["start_time"].to_i
+		end
+		def lobby
+			Lobby.new Storage.new.get("lobbies", {"id" => @raw["lobby_type"]}) 
+		end
+		def game_mode
+			GameMode.new Storage.new.get("game_modes", {"id" => @raw["game_mode"]})
+		end
+		def region
+			Region.new Storage.new.get("regions", {"id" => @raw["cluster"]})
+		end
 	end
 end
