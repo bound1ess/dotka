a6b7d4fcc59533c0696bd6391d179a3f9d2292b1
Ilya <bound1ess.dev@gmail.com>
2014-12-11 14:48:31 +0600

    Add Dotka::match method and test it.

--- specs/dotka_spec.rb
+++ specs/dotka_spec.rb
@@ -1,8 +1,10 @@
 require_relative "../src/dotka"
+require_relative "../src/dotka/match"
+require "rest-client"
 
 RSpec.describe Dotka do
 
-	before :each do
+	before :all do
 		@dotka = Dotka.new
 	end
 
@@ -10,5 +12,20 @@ RSpec.describe Dotka do
 		@dotka.set_api_key "some-key"
 		expect(@dotka.get_api_key).to eq("some-key")
 	end
+	
+	it "loads a match" do
+		allow(RestClient).to receive("get") {
+			FakeResponse.new(200, '{"result": {"match_id": 123456789}}')
+		}
+		expect(match = @dotka.match(123456789)).to be_instance_of(DotkaM::Match)
+		expect(match.id).to eq(123456789)
+	end
 
+end
+
+class FakeResponse
+	attr_reader "code", "to_str" 
+	def initialize code, body
+		@code, @to_str = code, body
+	end
 end
--- src/dotka.rb
+++ src/dotka.rb
@@ -1,15 +1,24 @@
-class Dotka
-	
+require "rest-client"
+require_relative "dotka/match"
+require "json"
+
+class Dotka	
 	def initialize
 		@key = nil
 	end
-
 	def set_api_key key
 		@key = key
 	end
-
 	def get_api_key
 		@key
 	end
-
+	def match id
+		raise "Please set up the API key!" unless not @key.nil?
+		response = RestClient.get(
+			"https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001",
+			{"params" => {"key" => @key, "match_id" => id}}
+		)
+		raise "Can not load a match with ID #{id}." unless response.code == 200
+		DotkaM::Match.new(JSON.parse(response.to_str)["result"])
+	end
 end
